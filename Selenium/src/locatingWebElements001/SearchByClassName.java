e05a12b562346cc403d5f93190e3bbb7c5c6c4e3
madhank93 <maddymadhan946@gmail.com>
2018-04-08 03:15:01 +0530

    Initial Commit.

---
+++ Selenium/src/locatingWebElements001/SearchByClassName.java
@@ -0,0 +1,28 @@
+package locatingWebElements001;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SearchByClassName {
+	
+	static final String chromeDriver = "C:\\Users\\Madhan\\git\\Selenium\\Selenium\\Google Chrome Driver\\";
+	//static final String geckoDriver = "C:\\Users\\Madhan\\git\\Selenium\\Selenium\\Mozilla GeckoDriver\\"; // For Fire fox browser
+	static WebDriver driver;
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", chromeDriver+"chromedriver.exe");
+		driver = new ChromeDriver();
+		// For Fire fox browser
+		// System.setProperty("webdriver.gecko.driver", geckoDriver+"geckodriver.exe");
+		
+		driver.get("http://book.theautomatedtester.co.uk/chapter1");
+		WebElement linkText = driver.findElement(By.className("loadajax"));
+		linkText.click();
+		
+		System.out.println("Done");
+	}
+}
---
+++ Selenium/src/locatingWebElements001/SearchByLinkText.java
@@ -0,0 +1,29 @@
+package locatingWebElements001;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SearchByLinkText {
+	
+	static final String chromeDriver = "C:\\Users\\Madhan\\git\\Selenium\\Selenium\\Google Chrome Driver\\";
+	//static final String geckoDriver = "C:\\Users\\Madhan\\git\\Selenium\\Selenium\\Mozilla GeckoDriver\\"; // For Fire fox browser
+	static WebDriver driver;
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", chromeDriver+"chromedriver.exe");
+		driver = new ChromeDriver();
+		// For Fire fox browser
+		// System.setProperty("webdriver.gecko.driver", geckoDriver+"geckodriver.exe");
+		
+		driver.get("http://book.theautomatedtester.co.uk/chapter2");
+		WebElement indexLink = driver.findElement(By.linkText("Index"));
+		indexLink.click();
+		
+		System.out.println("Done");
+		
+	}
+}
