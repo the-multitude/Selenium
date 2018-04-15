eb3969c7a706858adc71526c832a69c3f486b908
madhank93 <maddymadhan946@gmail.com>
2018-04-15 14:27:09 +0530

    package name change

--- Selenium/src/keyboardInteractions004/ActionBuildPerform.java
+++ Selenium/src/keyboardInteractions004/ActionBuildPerform.java
@@ -1,34 +1,37 @@
-package advancedInteractions004;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+package keyboardInteractions004;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
-public class ActionBuildPerform2 {
+import actionsOnWebElements002.SendKeys;
+
+public class ActionBuildPerform {
 	
 	static final String chromeDriver = System.getProperty("user.dir")+ "\\Google Chrome Driver\\";
 	//static final String geckoDriver = System.getProperty("user.dir")+ "\\Mozilla GeckoDriver\\"; // For Fire fox browser
-	static WebDriver driver;
+	static WebDriver driver;	
 	
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", chromeDriver+"chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://the-internet.herokuapp.com/key_presses");
-		
-		// Add all actions into the Actions builder.
-		Actions builder = new Actions(driver);
-		builder.keyDown(Keys.SHIFT).sendKeys("m").keyUp(Keys.SHIFT);
 
-		// Perform action.
-		builder.perform();
+		// Add action(s) into the Actions builder.
+		Actions builder = new Actions(driver);
+		builder.sendKeys("3");
+
+		// Generate composite action.
+		Action compositeAction = builder.build();
+
+		// Perform composite action.
+		compositeAction.perform();
 		
 		Thread.sleep(2000);
 		driver.quit();
 		
 	}
+
 }
--- Selenium/src/keyboardInteractions004/ActionBuildPerform2.java
+++ Selenium/src/keyboardInteractions004/ActionBuildPerform2.java
@@ -1,37 +1,34 @@
-package advancedInteractions004;
+package keyboardInteractions004;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
-import actionsOnWebElements002.SendKeys;
-
-public class ActionBuildPerform {
+public class ActionBuildPerform2 {
 	
 	static final String chromeDriver = System.getProperty("user.dir")+ "\\Google Chrome Driver\\";
 	//static final String geckoDriver = System.getProperty("user.dir")+ "\\Mozilla GeckoDriver\\"; // For Fire fox browser
-	static WebDriver driver;	
+	static WebDriver driver;
 	
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", chromeDriver+"chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://the-internet.herokuapp.com/key_presses");
-
-		// Add action(s) into the Actions builder.
+		
+		// Add all actions into the Actions builder.
 		Actions builder = new Actions(driver);
-		builder.sendKeys("3");
+		builder.keyDown(Keys.SHIFT).sendKeys("m").keyUp(Keys.SHIFT);
 
-		// Generate composite action.
-		Action compositeAction = builder.build();
-
-		// Perform composite action.
-		compositeAction.perform();
+		// Perform action.
+		builder.perform();
 		
 		Thread.sleep(2000);
 		driver.quit();
 		
 	}
-
 }
